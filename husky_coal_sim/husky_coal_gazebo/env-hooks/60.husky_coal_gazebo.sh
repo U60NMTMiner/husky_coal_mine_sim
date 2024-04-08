6ca16e9e9a00c94dacc4c5a18a67b07f14788cdd
billisandr <vandroulakis100@gmail.com>
2024-04-08 04:57:57 -0600

    Added husky_coal_navigation - unfinished

--- husky_coal_sim/husky_coal_gazebo/env-hooks/60.husky_coal_gazebo.sh
+++ husky_coal_sim/husky_coal_gazebo/env-hooks/60.husky_coal_gazebo.sh
@@ -2,3 +2,4 @@ export GAZEBO_MODEL_PATH=$(catkin_find --first-only husky_coal_gazebo worlds/ 2>
 #export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$(catkin_find --first-only husky_coal_gazebo worlds/coal_mine_playpen.world 2>/dev/null)
 #export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$(rospack find husky_coal_gazebo)/worlds
 #export HUSKY_GAZEBO_DESCRIPTION=$(catkin_find --first-only husky_coal_gazebo worlds/coal_mine_playpen.world 2>/dev/null)
+
